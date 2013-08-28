20217a24ac2ed389522c54ddb4d8c31a608f1acd
Pulser <pulser@xda-developers.com>
2013-08-28 11:32:26 +0100

    manta: Modify for custom build
    Change-Id: I6743d799da519c90473d061b305965c6b3b00f90

--- aosp_manta.mk
+++ aosp_manta.mk
@@ -13,6 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-$(call inherit-product, device/samsung/manta/full_manta.mk)
+$(call inherit-product, device/samsung/manta/custom_manta.mk)
 
 PRODUCT_NAME := aosp_manta
