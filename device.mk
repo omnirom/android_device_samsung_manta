3e3d839c10939d94cf5feb3ae59579a991fb891c
Menno van Grinsven <scanno71@gmail.com>
2015-03-03 21:51:11 +0100

    f2fs add filemangement tools and try yo mount f2fs
    Change-Id: Ib32da2ccfd1d2a37515274be2bf873b4c9073309

--- device.mk
+++ device.mk
@@ -158,7 +158,9 @@ PRODUCT_PACKAGES += \
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
-    e2fsck
+    e2fsck \
+    fsck.f2fs \
+    mkfs.f2fs
 
 PRODUCT_PROPERTY_OVERRIDES := \
     wifi.interface=wlan0 \
